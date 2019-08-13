05b455f2424071260d569e8b6360b465564a9ede
Joebidden1273 <52731570+Joebidden1273@users.noreply.github.com>
2019-08-13 08:09:01 +0300

    Revert "Joeone"

--- Node/CreateFiles/open.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-//create an empty file named mynewfile2.txt:
-fs.open('mynewfile2.txt', 'w', function (err, file) {
-  if (err) throw err;
-  console.log('Saved!');
-});
--- Node/DeleteFiles/unlink.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-fs.unlink('mynewfile3.txt',function(err){
-    if (err) throw err;
-    console.log('File deleted');
-
-});
